c055e18bcaeba6442f223d447ba786d2800a5540
Mauro <maurolschiavinato@gmail.com>
2021-03-22 11:26:03 -0300

    arreglo errores visuales en pdf

--- Guias/src/compile.sh
+++ Guias/src/compile.sh
@@ -1,4 +1,9 @@
-for f in *.md; do
-	echo "Compilando $f"
-	pandoc "$f" --from markdown+latex_macros -o "../practica${f%.*} - solucion.pdf"
-done
+if [ $# != 0 ]; then
+	echo "Compilando $1"
+	pandoc "$1" --from markdown+latex_macros -o "../practica${1%.*} - solucion.pdf"
+else
+	for f in *.md; do
+		echo "Compilando $f"
+		pandoc "$f" --from markdown+latex_macros -o "../practica${f%.*} - solucion.pdf"
+	done
+fi
